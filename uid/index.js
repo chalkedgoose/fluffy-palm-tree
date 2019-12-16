d6433f97c8db553b62f01920fde42f6b457ea24c
Carlos Alba <soradev4@gmail.com>
2019-12-16 15:16:59 -0800

    URL Safe UID

--- uid/index.js
+++ uid/index.js
@@ -1,9 +1,7 @@
-
-/** Generates a Unique ID String */
-module.exports.generateID = function(){
-    // A random number between min and max used to determine ID length 
-    const randInt =  (max, min) => Math.floor(Math.random() * (max - min + 1)) + min
-    return [...Array(randInt(3, 5)).keys()]
-    .map(String.fromCharCode(randInt(32, 100)))
-    .join('');
+/** Generates a URL Safe Unique ID String */
+module.exports.generateID = function () {
+   const bits = (Math.random() * 46656) | 0;
+   const pre = `000${bits.toString(36)}`.slice(-3);
+   const post = `000${bits.toString(36)}`.slice(-3);
+   return pre + post;
 }
