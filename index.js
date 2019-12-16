b4a576c556c754324e196046c97c1d4624236467
Carlos Alba <soradev4@gmail.com>
2019-12-16 13:47:35 -0800

    route changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express'); 
 const app = express(); 
 
-app.use(express.urlencoded());
+app.use(express.static('public'));
+
+app.get('/', (_, res) => res.status(200).sendFile('index.html'));
+
+app.post('/shorten', (req, res) => {
+    console.log(req.body);
+    return res.status(201).redirect('/');
+});
 
 app.listen(3000, () => console.log('URL Shortening Application Running at Port:3000'));
