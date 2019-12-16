f8ce280b31b1d6955ef5e668da29d7f6b57e5900
Carlos Alba <soradev4@gmail.com>
2019-12-16 14:33:14 -0800

    server changes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express'); 
+const bodyParser = require('body-parser');
 const app = express(); 
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(express.json());
-
-app.get('/', (_, res) => res.status(200).sendFile('index.html'));
 
 app.post('/shorten', (req, res) => {
+    console.log(req.body)
     return res.status(201).json(req.body);
 });
+
+app.get('/', (_, res) => {
+    return res.status(200).sendFile('index.html');
+});
+
 
 app.listen(3000, () => console.log('URL Shortening Application Running at Port:3000'));
