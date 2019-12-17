da7ae222e60cda3c67c18dc7cc6b6f331bd47f7e
Carlos Alba <soradev4@gmail.com>
2019-12-16 16:07:59 -0800

    shorten without recreating

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const { URLMappingModel } = require('./model');
 const { generateID } = require('./uid');
 
-mongoose.connect('mongodb://localhost:27017/critical-mass-url-redirection', { useNewUrlParser: true })
+mongoose
+.connect('mongodb://localhost:27017/critical-mass-url-redirection',
+ { useNewUrlParser: true, useCreateIndex: true });
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.post('/shorten', (req, res) => {
+app.post('/shorten', async (req, res) => {
     try {
-        const uid = generateID();
-        return res.status(201).json({
-            href: `http://localhost:3000/${uid}`
-        });
+        const lookup = await URLMappingModel.findOne({ href: req.body.href }).exec();
+        if(lookup !== null){
+            return res.status(200).json({
+                href: `http://localhost:3000/${lookup.uid}`
+            })
+        }
+        else if(lookup === null){
+            const uid = generateID();
+            const urlMapping = new URLMappingModel({ uid, href: req.body.href })
+            await urlMapping.save()
+            return res.status(201).json({
+                href: `http://localhost:3000/${uid}`
+            });
+        }
     } catch (error) {
         console.log(error.message)
         return res.status(500).send(error.message);
