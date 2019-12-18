a08896c9b1d66101962c8a5e8f5059acac44d12f
Carlos Alba <soradev4@gmail.com>
2019-12-18 08:53:24 -0800

    public

---
+++ public/scripts/index.js
@@ -0,0 +1,26 @@
+hrefInput = document.querySelector('#href-input');
+linkOutput = document.querySelector('#link-output');
+
+// sends request to shortening endpoint and modifies the DOM.
+async function fetchUrl() {
+    try {
+        console.log({
+            href: hrefInput.value
+        })
+        const data = await fetch('/shorten', {
+            method: 'POST',
+            headers: {
+                "Same-Source": true,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                href: hrefInput.value
+            })
+        });
+        const parsed = await data.json();
+        linkOutput.textContent = parsed.href;
+    } catch (error) {
+        console.log(error);
+        linkOutput.textContent = error.message
+    }
+}
